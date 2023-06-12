ac52111632e07e77c1da465dc932257ed38f216e
KARDELEN <120568669+kcan25@users.noreply.github.com>
2023-06-12 21:17:34 +0300

    DAKS
    en son hali

--- yeniproje.py
+++ yeniproje.py
@@ -27,12 +27,37 @@ while True:
 
     if secim == 1:
         print("Sisteme Üye Olma İşlemi")
+         ad = input("Adınızı giriniz: ")
+        soyad = input("Soyadınızı giriniz: ")
+        kullanici_adi = input("Kullanıcı adı belirleyiniz: ")
+        sifre = input("Şifre belirleyiniz: ")
+        email = input("E-posta adresinizi giriniz: ")
+
+        yeni_kullanici = pd.DataFrame({'Ad': [ad], 'Soyad': [soyad], 'Kullanıcı Adı': [kullanici_adi], 'Şifre': [sifre], 'E-posta': [email]})
+        df = df.append(yeni_kullanici, ignore_index=True)
+        df.to_excel('veriler.xlsx', index=False)
 
     elif secim == 2:
         print("Sisteme Giriş Yapma İşlemi")
+         print("Sisteme Giriş Yapma İşlemi")
+        kullanici_adi = input("Kullanıcı adınızı giriniz: ")
+        sifre = input("Şifrenizi giriniz: ")
+        if kullanici_adi in df['Kullanıcı Adı'].values and sifre in df['Şifre'].values:
+            print("Giriş başarılı.")
+        else:
+            print("Giriş başarısız. Lütfen tekrar deneyin.")
        
     elif secim == 3:
         print("Şifremi Unuttum İşlemi")
+        kullanici_bilgisi = input("Kullanıcı adınızı veya e-posta adresinizi giriniz: ")
+
+        if kullanici_bilgisi in df['Kullanıcı Adı'].values or kullanici_bilgisi in df['E-posta'].values:
+            yeni_sifre = "yeni_sifre"  
+            sifre_sifirlandi = True  
+            print("Şifreniz sıfırlandı. Yeni şifreniz e-posta adresinize gönderildi.")
+        else:
+            sifre_sifirlandi = False  
+            print("Kullanıcı bulunamadı. Lütfen geçerli bir kullanıcı adı veya e-posta adresi girin.")
 
     elif secim == 4:
         print("Çıkış Yapılıyor...")
@@ -56,7 +81,16 @@ while True:
            
     elif secim == 6:
         print("Sistemdeki hastalık bilgisi güncelleme işlemi")
-        
+        kullanici_adi = input("Kullanıcı adınızı giriniz: ")
+    
+       if kullanici_adi in df['Kullanıcı Adı'].values:
+        hastalik = input("Güncel hastalık bilgisini giriniz: ")
+        df.loc[df['Kullanıcı Adı'] == kullanici_adi, 'Hastalık'] = hastalik
+        df.to_excel('veriler.xlsx', index=False)
+        print("Hastalık bilgisi güncellendi.")
+       else:
+        print("Kullanıcı bulunamadı. Lütfen geçerli bir kullanıcı adı girin.")
+
     else:
         print("Geçersiz işlem seçimi! Lütfen tekrar deneyin.")
 
